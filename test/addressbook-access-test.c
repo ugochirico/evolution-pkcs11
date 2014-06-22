c85caad1bb353a7852d281210ed5d94800e3559c
Watson Yuuma Sato <yuuma.sato@gmail.com>
2014-06-22 00:39:27 -0300

    addressbook-access-test tests accesst to contacts in addressbook
    It searches contacts whose email contains the passed argument.

--- test/addressbook-access-test.c
+++ test/addressbook-access-test.c
@@ -5,10 +5,41 @@
 int main (int argc, char **argv)
 {
 	GError *error;
-	ESource *source = NULL;
+	ESourceRegistry *registry;
+	EClientCache *client_cache;
+	EBookClient *client_addressbook;
+	GList *addressbooks, *aux_addressbooks;
+	GSList *contacts, *it;
+	gchar *query_string, *email;
+	gboolean status;
+	gchar *fullname;
 
-	e_shell_get_default();
-	printf("Hello addressbook-access-test\n");
+	if (argc != 2) return 1;
+
+	email = argv[1];
+	 
+	registry = e_source_registry_new_sync (NULL, &error);
+
+	client_cache = e_client_cache_new (registry);
+	addressbooks = e_source_registry_list_enabled (registry, E_SOURCE_EXTENSION_ADDRESS_BOOK);
+
+	query_string = g_strdup_printf ("(contains \"email\" \"%s\")", email);
+
+	aux_addressbooks = addressbooks;
+	while (aux_addressbooks != NULL) {
+
+		client_addressbook = (EBookClient *) e_client_cache_get_client_sync (client_cache, (ESource *) aux_addressbooks->data, E_SOURCE_EXTENSION_ADDRESS_BOOK, NULL, &error);
+
+		status = e_book_client_get_contacts_sync (client_addressbook, query_string, &contacts, NULL, NULL);
+		if (status && contacts != NULL) {
+			for (it = contacts; it != NULL; it = it->next){
+				fullname  = e_contact_get (it->data, E_CONTACT_FULL_NAME);
+				printf ("Fullname: %s\n", fullname);
+			}
+		} 
+
+		aux_addressbooks = aux_addressbooks->next;
+	}
 
 	return 0;
 }
