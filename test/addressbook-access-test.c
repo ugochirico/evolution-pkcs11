d0661ee968a664194f3a6c672a4aa317483992fb
Watson Yuuma Sato <yuuma.sato@gmail.com>
2014-07-10 22:21:10 -0300

    Now test application also queries for contacts with certificates. Certificate found during search now is printed in hexa format;

--- test/addressbook-access-test.c
+++ test/addressbook-access-test.c
@@ -4,17 +4,18 @@
 
 int main (int argc, char **argv)
 {
-	GError *error;
+	GError *error = NULL;
 	ESourceRegistry *registry;
 	EClientCache *client_cache;
 	EBookClient *client_addressbook;
 	GList *addressbooks, *aux_addressbooks;
 	GSList *contacts, *it;
-	EBookQuery *query;
+	EBookQuery *final_query, *query_mail, *query_cert;
 	gchar *query_string, *email;
 	gboolean status;
 	gchar *fullname;
 	EContactCert *cert;
+	gsize i;
 
 	if (argc != 2) return 1;
 
@@ -25,8 +26,11 @@ int main (int argc, char **argv)
 	client_cache = e_client_cache_new (registry);
 	addressbooks = e_source_registry_list_enabled (registry, E_SOURCE_EXTENSION_ADDRESS_BOOK);
 
-	query = e_book_query_field_test (E_CONTACT_EMAIL, E_BOOK_QUERY_CONTAINS, email);
-	query_string = e_book_query_to_string (query);
+	query_mail = e_book_query_field_test (E_CONTACT_EMAIL, E_BOOK_QUERY_CONTAINS, email);
+	query_cert = e_book_query_field_exists (E_CONTACT_X509_CERT);
+	final_query = e_book_query_andv (query_cert, query_mail, NULL);
+
+	query_string = e_book_query_to_string (final_query);
 	
 	printf("Query String: %s\n", query_string);
 
@@ -43,7 +47,10 @@ int main (int argc, char **argv)
 				cert = e_contact_get (it->data, E_CONTACT_X509_CERT);
 				if (cert != NULL) {
 					printf ("Size: %lu\n", cert->length);
-					printf ("Cert: %s\n", cert->data);
+					printf ("Cert: \n");
+					for (i = 0; i < cert->length; i++) {
+						printf ("%02X:", ((unsigned char *)cert->data)[i]);
+					}
 				}
 			}
 		} 
